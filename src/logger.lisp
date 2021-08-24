7c6c2c3e5cc86923afa7cdf9dbe66225a8510ea2
Alexey Veretennikov <fourier@protonmail.ch>
2021-08-24 16:35:31 +0200

    Updated logger

--- src/logger.lisp
+++ src/logger.lisp
@@ -1,9 +1,33 @@
 (in-package numen)
 
-(defparameter *logger-channel* (make-instance 'chanl:channel)
+(defpackage #:numen.logger
+  (:documentation "Logger package")
+  (:use #:cl #:alexandria)
+  (:export
+   logger-init
+   logger-stop
+   logger-set-level
+   inf
+   dbg
+   err
+   *logger-level*))
+
+(in-package #:numen.logger)
+
+(defparameter *logger-channel* nil
   "Channel for logging")
 
+(defparameter *logger-level* 2
+  "Level for logging.
+0 means nothing,
+1 only errors,
+2 info and errors,
+3 everything")
+
 (defun logger-init ()
+  (if *logger-channel*
+      (chanl:send *logger-channel* :stop)
+      (setf *logger-channel* (make-instance 'chanl:channel)))
   (chanl:pexec ()
    (loop for msg = (chanl:recv *logger-channel*)
          until (eq msg :stop)
@@ -14,11 +38,29 @@
 (defun logger-stop ()
   (chanl:send *logger-channel* :stop :blockp nil))
 
-(defmacro info(message &rest args)
+(defun logger-set-level (level)
+  (setf *logger-level* level))
+
+
+(defmacro msg(message lvl accept-lvl-num &rest args)
   (let ((module-name (package-name *package*)))
-    `(info-message (concatenate 'string "[" ,module-name "] " ,message) ,@args)))
+    `(if (> numen.logger::*logger-level* ,accept-lvl-num)
+         (info-message (concatenate 'string "[" ,module-name "] " ,lvl ": " ,message) ,@args)
+         (values))))
+
+(defmacro inf(message &rest args)
+  `(msg ,message "I" 1 ,@args))
+
+(defmacro dbg(message &rest args)
+  `(msg ,message "D" 2 ,@args))
+
+(defmacro err(message &rest args)
+  `(msg ,message "E" 0 ,@args))
 
 (defun info-message (message &rest args)
   (let ((msg (apply #'format (append (list nil message) args))))
     (chanl:send *logger-channel* msg :blockp nil))
   (values))
+
+
+(inf "hello")
--- src/streamlistener.lisp
+++ src/streamlistener.lisp
@@ -1,7 +1,9 @@
+(in-package numen)
+
 (defpackage #:numen.streamlistener
   (:documentation "A connection to the Frostbite Client streaming service")
-  (:use #:cl #:alexandria))
-;;  (:export version-string))
+  (:use #:cl #:alexandria #:numen.logger)
+  (:export start stop))
 
 (in-package #:numen.streamlistener)
 
@@ -11,48 +13,48 @@
 (defparameter *stream-port* 10001
   "Port number for the Frostbite streaming server")
 
-(defparameter *interrupted* t
+(defparameter *stopped* t
   "Set this parameter to T to abort the thread execution")
 
 (defun stop ()
-  (unless *interrupted*
-    (numen::info "Stopping the StreamListener")
-    (setf *interrupted* t)
+  (unless *stopped*
+    (inf "Stopping the StreamListener")
+    (setf *stopped* t)
     (sleep 1.5)
-    (numen::info "Stopped StreamingListener")))
+    (inf "Stopped StreamingListener")))
 
 (defun start (&optional (on-received-callback nil))
-  (numen::info "Start StreamingListener")
-  (when  *interrupted*
-    (numen::info "No previous StreamListener, starting new")
+  (inf "Start StreamingListener")
+  (when  *stopped*
+    (inf "No previous StreamListener, starting new")
     (bt:make-thread
      (lambda () (do-start on-received-callback))
      :name "StreamListener")))
 
 (defun do-start (on-received-callback)
-  (setf *interrupted* nil)
-  (numen::info "Starting the connect loop")
-  (loop while (not *interrupted*)
+  (setf *stopped* nil)
+  (inf "Starting the connect loop")
+  (loop while (not *stopped*)
         do
-        (numen::info "Trying to connect to the server on port ~a..." *stream-port*)
+        (inf "Trying to connect to the server on port ~a..." *stream-port*)
         (ignore-errors (connect-and-read on-received-callback))
         (sleep 1))
-  (setf *interrupted* t)
-  (numen::info "Connect loop finished"))
+  (setf *stopped* t)
+  (inf "Connect loop finished"))
 
 (defun connect-and-read (on-received-callback)
   (usocket:with-client-socket (socket stream *stream-server* *stream-port* :timeout 2)
     (handler-case
         (unwind-protect
-            (loop while (not *interrupted*)
+            (loop while (not *stopped*)
                   do
                   (usocket:wait-for-input socket :ready-only t :timeout 0.1 )
                   (let ((line (read-until-newline stream)))
                     (when (and line on-received-callback)
                       (funcall on-received-callback line))))
           (usocket:socket-close socket))
-      (error (e) (numen::info "Error: ~a" e))))
-  (numen::info "connect-and-read finished"))
+      (error (e) (inf "Error: ~a" e))))
+  (inf "connect-and-read finished"))
 
 (defun read-until-newline (&optional (stream *standard-input*))
   "Try to do nonblocking read of the stream until the newline and return a line"
